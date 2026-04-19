04f674058d3d53778393df8b3d4953301ba3c52d
njambi101 <grace.nganga@student.moringaschool.com>
2026-04-19 18:34:03 +0300

    project

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+require('datejs')
+
+
+let users =["Jim3","Pam5","Dwight77"];
+let users2=["Michael6","Eleanor22","Chidi202"];
+let users3=["Jack_jack","Julia_Oreo", "Bill_bore"];
+
+const combineUsers = ()=>{
+      const username = {
+        users4: [users,users2,users3]}
+      console.log(username)
+
+      const combineobj = {
+        merge_date : Date.today().toString("M/d/yyyy")
+        
+      };
+      console.log(combineobj.merge_date)
+}
+
+
+combineUsers();
+
+
+
+
 
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+
+  
+}
+
